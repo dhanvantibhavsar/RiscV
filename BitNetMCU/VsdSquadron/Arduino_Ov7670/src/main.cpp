fb99ddf7d02be9bbcb61e9f47f005a70c98fca96
dhanvantibhavsar <dhanvantibhavsar98@gmail.com>
2024-06-10 20:28:11 +0530

    edited OV7670 code to output 16x16 image

--- BitNetMCU/VsdSquadron/Arduino_Ov7670/src/main.cpp
+++ BitNetMCU/VsdSquadron/Arduino_Ov7670/src/main.cpp
@@ -9,12 +9,15 @@ void setup() {
   CLKPR = 0x80; // enter clock rate change mode
   CLKPR = 0; // set prescaler to 0. WAVGAT MCU has it 3 by default.
 
+  pinMode(8 , INPUT_PULLUP );
   initializeScreenAndCamera();
 }
 
 
 void loop() {
-  processFrame();
+  if(digitalRead(8) == LOW) {
+    processFrame();
+  }
 }
 
 
